aa54b0f8cc83c05a876d7a173943d1c3e8dd7919
x1n9snv0 <vichi_7@sina.com>
2017-09-13 01:00:21 +0800

    test first css class, body and time

--- getTime.php
+++ getTime.php
@@ -4,8 +4,6 @@ header("cache-control:no-cache,must-revalidate");
 header("Content-Type:text/html;charset=utf-8");
 $showtime = '<span class="time digit">';
 $showtime += date("H:i");
-$showtime += '</span><span class="time sec">';
-$showtime += date("s");
-$showtime += '</span>';
+$showtime += '</span>
 echo $showtime;
 ?>
