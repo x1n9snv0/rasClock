46f7d047dff92918b8d55abf47591e8d01170327
x1n9snv0 <vichi_7@sina.com>
2017-09-12 18:12:22 +0800

    test client info

--- index.php
+++ index.php
@@ -4,34 +4,38 @@
 <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
 <title>rasClock</title>
 </head>
+
 <body>
-<input type="button" value="开始显示时间" id="go" onclick="start()" />
-<p>当前时间：<font color="red"><span id="showtime"></span></font></p>
+    <input type="button" value="开始显示时间" id="go" onclick="start()" />
+    <p>当前时间：<font color="red"><span id="showtime"></span></font></p>
 </body>
+
 <script type="text/javascript">
-var xmlHttp;
-function createXMLHttpRequest(){
- if(window.ActiveXObject){
-  xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
- }
- else if(window.XMLHttpRequest){
-  xmlHttp = new XMLHttpRequest();
- }
-}
-function start(){
- createXMLHttpRequest();
- var url="getTime.php";
- xmlHttp.open("GET",url,true);
- xmlHttp.onreadystatechange = callback;
- xmlHttp.send(null);
-}
-function callback(){
- if(xmlHttp.readyState == 4){
-  if(xmlHttp.status == 200){
-   document.getElementById("showtime").innerHTML = xmlHttp.responseText;
-   setTimeout("start()",1000);
-  }
- }
-}
+    var xmlHttp;
+    function createXMLHttpRequest(){
+        if(window.ActiveXObject){
+            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        else if(window.XMLHttpRequest){
+        xmlHttp = new XMLHttpRequest();
+        }
+    }
+
+    function start(){
+        createXMLHttpRequest();
+        var url="getTime.php";
+        xmlHttp.open("GET",url,true);
+        xmlHttp.onreadystatechange = callback;
+        xmlHttp.send(null);
+    }
+
+    function callback(){
+        if(xmlHttp.readyState == 4){
+            if(xmlHttp.status == 200){
+                document.getElementById("showtime").innerHTML = xmlHttp.responseText;
+                setTimeout("start()",1000);
+            }
+        }
+    }
 </script>
 </html>
