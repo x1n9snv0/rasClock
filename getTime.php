dd2e6ade2196ee3cdcb564e0e5cf4bdb0124dc01
x1n9snv0 <vichi_7@sina.com>
2017-09-13 01:02:40 +0800

    test first css class, body and time

--- getTime.php
+++ getTime.php
@@ -2,8 +2,6 @@
 date_default_timezone_set('prc');
 header("cache-control:no-cache,must-revalidate");
 header("Content-Type:text/html;charset=utf-8");
-$showtime = '<span class="time digit">';
-$showtime += date("H:i:s");
-$showtime += '</span>';
+$showtime = date("H:i:s");
 echo $showtime;
 ?>
