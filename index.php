2feed3cf6f5e6ac4390e7ca9d5fd4b7405d42066
x1n9snv0 <vichi_7@sina.com>
2017-09-13 18:58:08 +0800

    test first css class, body and time

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 <body>
     <div class="time">
     <script type="text/javascript">start();</script>
-    <span id="showtime"></span>
+    <span class="day" id="showtime"></span>
     </div>
 </body>
 
