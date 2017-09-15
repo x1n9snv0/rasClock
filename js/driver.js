10a8c735cb860c69498319b4520e8f250f5d1aec
x1n9snv0 <vichi_7@sina.com>
2017-09-15 19:57:54 +0800

    test weather update

--- js/driver.js
+++ js/driver.js
@@ -54,7 +54,7 @@ function d_callback(){
             document.getElementById("hhmm").innerHTML = h + ":" + i;
             document.getElementById("ss").innerHTML = s;
             document.getElementById("date").innerHTML = Y + "年" + m + "月" + d + "日 星期" + N;
-            if (i=="56" && s=="20"){
+            if (i=="58" && s=="20"){
                 weather.update()
             }
             setTimeout("driver()", 1000);
