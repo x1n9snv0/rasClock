ffbc1b87f989e8244c9908af08424bb9259ed8a3
x1n9snv0 <vichi_7@sina.com>
2017-09-16 11:34:30 +0800

    adjust the color of day, night and second

--- js/driver.js
+++ js/driver.js
@@ -54,7 +54,7 @@ function d_callback(){
             document.getElementById("hhmm").innerHTML = h + ":" + i;
             document.getElementById("ss").innerHTML = s;
             document.getElementById("date").innerHTML = Y + "年" + m + "月" + d + "日 星期" + N;
-            if (s=="40"){
+            if (s.subString(1)=="7"){
                 weather.update();
             }
             tip.update();
