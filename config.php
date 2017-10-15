b8aeb2109ceea07106fd9e85d85a859df41216a3
x1n9snv0 <vichi_7@sina.com>
2017-10-15 18:51:38 +0800

    test add provence

--- config.php
+++ config.php
@@ -12,49 +12,31 @@
 		<link rel="stylesheet" type="text/css" href="css/config/cs-select.css" />
 		<link rel="stylesheet" type="text/css" href="css/config/cs-skin-elastic.css" />
 		<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+		<script type="text/javascript" src="js/buildconfig.js"></script>
 	</head>
-	<script type="text/javascript">
-	    $(document).ready(function(){
-	        $("#slt_city").change(function(){
-	            var checkText=$("#slt_city").find("option:selected").text();
-                var checkValue=$("#slt_city").val();
-                var index=$("#slt_city").get(0).selectedIndex;
-                alert("Text: "+checkText + "\n" + "Value: " + checkValue + "\n" + "Index: " + index + "\n");
-	        });
-	    });
-	</script>
 	<body class="color-2">
 		<div class="container">
 			<header class="codrops-header">
-				<h1><span>Select your city</span> 选择你的城市 </h1>
+				<h1><span>Select your city</span>选择你的城市</h1>
 			</header>
 			<section>
-				<select id="slt_country" class="cs-select cs-skin-elastic">
-					<option value="" disabled selected>Select a Country</option>
+				<select id="slt_provence" class="cs-select cs-skin-elastic">
+					<option value="" disabled selected>选择省份</option>
+					<!--
 					<option value="france" data-class="flag-france">France</option>
 					<option value="brazil" data-class="flag-brazil">Brazil</option>
 					<option value="argentina" data-class="flag-argentina">Argentina</option>
 					<option value="south-africa" data-class="flag-safrica">South Africa</option>
-				</select>
-				<select id="slt_city" class="cs-select cs-skin-elastic">
-					<option value="" disabled selected>Select a Country</option>
-					<option value="france" data-class="flag-france">France</option>
-					<option value="brazil" data-class="flag-brazil">Brazil</option>
-					<option value="argentina" data-class="flag-argentina">Argentina</option>
-					<option value="south-africa" data-class="flag-safrica">South Africa</option>
+					-->
 				</select>
 			</section>
 		</div>
+		<script>addProvence()</script>
 		<script src="js/classie.js"></script>
 		<script src="js/selectFx.js"></script>
 		<script>
 			(function() {
-				[].slice.call(document.querySelectorAll('select#slt_country')).forEach(function(el){
-					new SelectFx(el);
-				});
-			})();
-			(function() {
-				[].slice.call(document.querySelectorAll('select#slt_city')).forEach(function(el){
+				[].slice.call(document.querySelectorAll('select#slt_provence')).forEach(function(el){
 					new SelectFx(el);
 				});
 			})();
---
+++ js/buildconfig.js
@@ -0,0 +1,27 @@
+function arrayHasKey(array, key){
+    for (var rawKey in array){
+        if (key == rawKey){return true;}
+    }
+    return false;
+}
+
+function addProvence(){
+    $.getJSON("js/city.json", function(data){
+        var i = 0;
+        var provArray = new Array();
+        while(i<data.length){
+            if (!arrayHasKey(provArray, data[i].pro_cn)){
+                push(provArray, data[i].pro_cn);
+            }
+            i++;
+        }
+        provArray.sort();
+        var slt = document.getElementById("slt_country");
+        for (var provence in provArray){
+            var opt = document.createElement("option");
+            opt.setAttribute("value", provence);
+            opt.innerText=provence;
+            slt.appendChild(opt);
+        }
+    });
+}
